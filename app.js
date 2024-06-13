988392017e53027b9068f3ca1d4bcbe83d638146
vusal965 <vusalbaxsiyev756@gmail.com>
2024-06-13 22:16:12 +0400

    Home work.js2

---
+++ app.js
@@ -0,0 +1,7 @@
+;
+let fullName = "Vusal Baxsiyev";
+console.log(fullName.split(" ")[0][0]);
+console.log(fullName.split(" ")[1][0]);
+
+let name = "Vusal Baxsiyev";
+console.log(name.split(" ")[1] + " " + name.split(" ")[0]);
